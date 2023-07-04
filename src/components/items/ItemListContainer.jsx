17ec5af7cb5e81cb3a590ab2292e4876865c8646
parzival2599 <jero.aguirre@gmail.com>
2023-07-04 17:25:41 -0300

    Segunda entrega

---
+++ src/components/items/ItemListContainer.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import AnimationSpin from "../AnimationSpin";
+import ItemList from "../items/ItemList";
+import { getProducts } from "../../utils/promises";
+
+const ItemListContainer = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const { category } = useParams();
+
+  useEffect(() => {
+    if (category === undefined) {
+      getProducts.then((response) => {
+        setProducts(response);
+        setLoading(false);
+      });
+    } else {
+      getProducts.then(
+        (response) =>
+          setProducts(response.filter((i) => category === i.category)),
+        setLoading(false)
+      );
+    }
+  }, [category]);
+
+  console.log(products);
+
+  return (
+    <div className="py-4 px-2 h-full border-b border-gray-300 mx-auto">
+      <ul className="flex flex-col items-center min-w-md justify-center lg:px-4 lg:flex-row lg:flex-wrap w-full">
+        {loading ? <AnimationSpin /> : <ItemList products={products} />}
+      </ul>
+    </div>
+  );
+};
+
+export default ItemListContainer;
